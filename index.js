5e46eb426d0744fe60a184663347a99b3ca01819
Peter D'Elia <petertdelia@gmail.com>
2021-01-27 11:29:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+const requestLogger = (req, res, next) => {
+  console.log('Method: ', req.method, 'Path: ', req.path, 'Body: ', req.body);
+  next();
+}
+
+app.use(express.json());
+app.use(requestLogger);
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = notes.find(note => note.id === Number(id));
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(note => note.id))
+  : 0;
+  return maxId + 1;
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  };
+
+  notes = notes.concat(note);
+  res.json(note);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter(note => note.id !== id);
+
+  res.status(204).end();
+});
+
+app.listen(port);
+console.log(`Server running on port ${port}`);
+
